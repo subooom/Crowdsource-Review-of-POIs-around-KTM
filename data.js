372f1b27fa09ac8df5174946e0b15bfdaa541113
subooom <subhamkharel12@gmail.com>
2018-07-07 19:59:18 +0545

    final

--- data.js
+++ data.js
@@ -1,31 +1,16 @@
-let database;
-let errors = [];
-let title, img;
-let index = 0;
-let generate = true;
-let loadingTemplate = `<div class="preloader-wrapper big active" style="position: relative; top: 40px"><div class="spinner-layer spinner-green-only"><div class="circle-clipper left"><div class="circle"></div></div><div class="gap-patch"><div class="circle"></div></div><div class="circle-clipper right"><div class="circle"></div></div></div></div>`;
+var database;
+var errors = [];
+var title, img;
+var index = 0;
+var generate = true;
+var loadingTemplate = '<div class="preloader-wrapper big active" style="position: relative; top: 40px"><div class="spinner-layer spinner-green-only"><div class="circle-clipper left"><div class="circle"></div></div><div class="gap-patch"><div class="circle"></div></div><div class="circle-clipper right"><div class="circle"></div></div></div></div>';
 
 function showExitScreen(){
   $('#card-item').animate({opacity: 0}, 500);
 
   setTimeout(function(){
-    $('#destination').html(`<div class="col s12 m7">
-    <h2 class="header">You're awesome! <img src="img/laugh.png" width="40px" />  </h2>
-    <div class="card horizontal">
-      <div class="card-image">
-        <img src="img/thankyou.jpg">
-      </div>
-      <div class="card-stacked">
-        <div class="card-content">
-          <h4>Thank you for your time</h4>
-          <p>As a departing gift, here's a nice picture of the Justice League America for you to download.</p>
-          <button id="download" class="btn waves-effect waves-light"> Download <i class="material-icons">cloud_download</i></button>
-          <p>Thank you for helping make the world a better place.</p>
-          <p>The data you provided will be used for good.</p>
-        </div>
-      </div>
-    </div>
-    </div>`);
+    $('#destination').html('<div class="col s12 m7"><h2 class="header">You are awesome! <img src="img/laugh.png" width="40px" />  </h2><div class="card horizontal"><div class="card-image"><img src="img/thankyou.jpg"></div><div class="card-stacked"><div class="card-content"><h4>Thank you for your time</h4><p>As a departing gift, here is a nice picture of the Justice League America for you to download.</p><button id="download" class="btn waves-effect waves-light"> Download <i class="material-icons">cloud_download</i></button><p>Thank you for helping make the world a better place.</p><p>The data you provided will be used for good.</p></div></div></div></div>');
+
   }, 520);
   setTimeout(function(){
     $('#download').click(function(e){
@@ -37,16 +22,16 @@ function showExitScreen(){
 }
 
 function sendData(){
-  let review_database = database.ref('reviews');
+  var review_database = database.ref('reviews');
 
-  let review_text = $("#review-text").val();
-  let input1 = $("#managed").is(':checked');
-  let input2 = $("#clean").is(':checked');
-  let input3 = $("#meditate").is(':checked');
-  let input4 = $("#tarmacked").is(':checked');
-  let title  = $('.header').html();
+  var review_text = $("#review-text").val();
+  var input1 = $("#managed").is(':checked');
+  var input2 = $("#clean").is(':checked');
+  var input3 = $("#meditate").is(':checked');
+  var input4 = $("#tarmacked").is(':checked');
+  var title  = $('.header').html();
 
-  let data = {
+  var data = {
     title: title,
     user_review: review_text,
     managed : input1,
@@ -56,7 +41,7 @@ function sendData(){
   }
   console.log('Saving data...');
 
-  let review = review_database.push(data, finished);
+  var review = review_database.push(data, finished);
   console.log("Firebase generated key: "+review.key);
 
   function finished(err) {
@@ -71,7 +56,7 @@ function sendData(){
 
 function generateItem(trips){
 
-  let trip = _getRandomItem(trips);
+  var trip = _getRandomItem(trips);
 
   if(!trip) return showExitScreen();
 
@@ -82,10 +67,14 @@ function generateItem(trips){
   } catch(e){
     generateItem(trips);
   }
-  let template = '<div id="card-item" class="col s12 m7"><h2 class="header">'+title.replace('Visit ', '')+'</h2><div class="card hoverable horizontal"><div class="card-image"><img class="trip-img" src="'+img+'"></div><div style="position: relative;left: 0px; width: 50%" class="card-stacked"><div class="card-content"><div class="input-field"><textarea id="review-text" class="materialize-textarea" rows="5"></textarea><label for="review-text">Review *(optional)</label><span id="error-span" class="helper-text red-text"></span></div><p><label><input type="checkbox" id="managed" /><span>Is it managed properly?</span></label></p><p><label><input type="checkbox" id="clean" /><span>Does it have a clean surrounding?</span></label></p><p><label><input type="checkbox" id="meditate" /><span>Can you meditate there?</span></label></p><p style="margin-bottom: 10px"><label><input type="checkbox" id="tarmacked" /><span>Is the road to get there tarmacked?</span></label></p><button id="next" class="btn waves-effect waves-light" name="action">Next<i class="material-icons right">send</i></button><button id="skip" class="deep-purple btn waves-effect waves-light"><span class="white-text text-darken-2"><i class="material-icons right">skip_next</i>Skip this one</span></button></div></div></div><div>';
+  var template = '<div id="card-item" class="col s12 m7"><h2 class="header">'+title.replace('Visit ', '')+'</h2><div class="card hoverable horizontal"><div class="card-image"><img class="trip-img" src="'+img+'"></div><div style="position: relative;left: 0px;" class="card-stacked"><div class="card-content"><div class="input-field"><textarea id="review-text" class="materialize-textarea" rows="5"></textarea><label for="review-text">Review *(optional)</label><span id="error-span" class="helper-text red-text"></span></div><p><label><input type="checkbox" id="managed" /><span>Is it managed properly?</span></label></p><p><label><input type="checkbox" id="clean" /><span>Does it have a clean surrounding?</span></label></p><p><label><input type="checkbox" id="meditate" /><span>Can you meditate there?</span></label></p><p style="margin-bottom: 10px"><label><input type="checkbox" id="tarmacked" /><span>Is the road to get there tarmacked?</span></label></p><button id="next" class="btn waves-effect waves-light" name="action">Next<i class="material-icons right">send</i></button><button id="skip" class="deep-purple btn waves-effect waves-light"><span class="white-text text-darken-2"><i class="material-icons right">skip_next</i>Skip this one</span></button></div></div></div><div>';
 
   setTimeout(function(){
     $('#destination').html(template);
+
+    if($( window ).width() <= 600){
+      $('.card').removeClass('horizontal');
+    }
     if(index>=3){
       $(".card-content").append('<a style="margin-top: 5px" id="exit" class=" waves-effect waves-light"><i class="material-icons right">exit_to_app</i>I am done, take me out</a>');
     }
@@ -102,17 +91,17 @@ function generateItem(trips){
     $('#next').click(function(e){
       e.preventDefault();
 
-      let review_text = $("#review-text").val();
-      let input1 = $("#managed").is(':checked');
-      let input2 = $("#clean").is(':checked');
-      let input3 = $("#meditate").is(':checked');
-      let input4 = $("#tarmacked").is(':checked');
+      var review_text = $("#review-text").val();
+      var input1 = $("#managed").is(':checked');
+      var input2 = $("#clean").is(':checked');
+      var input3 = $("#meditate").is(':checked');
+      var input4 = $("#tarmacked").is(':checked');
       if(review_text !== '' || input1 || input2 || input3 || input4){
         sendData();
         index = Math.floor(Math.random()*30);
         $('#card-item').animate({opacity: 0}, 500);
         setTimeout(function(){
-          $('#destination').html(`<h4 id="index" class="teal-text lighten-1"></h4>`+loadingTemplate);
+          $('#destination').html('<h4 id="index" class="teal-text lighten-1"></h4>'+loadingTemplate);
         }, 520);
         $('.preloader-wrapper').show();
         generateItem(trips);
@@ -128,10 +117,10 @@ function generateItem(trips){
   },2000);
 }
 
-const usedOnes = [];
+var usedOnes = [];
 
 function _getRandomItem(trips) {
-  let index;
+  var index;
 
   if(trips.length === usedOnes.length) return;
 
@@ -145,7 +134,6 @@ function _getRandomItem(trips) {
 }
 
 $(document).ready(function() {
-
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBMiFlDe29ao7r8Gj9HnC6oznOrb9ZG7lM",
@@ -158,27 +146,28 @@ $(document).ready(function() {
   firebase.initializeApp(config);
 
   database = firebase.database();
-  (async function(){
+  (function(){
     try {
       $('#destination').html(loadingTemplate);
 
-      const { data } = await axios.get('https://www.thrillophilia.com/places-to-visit-in-kathmandu');
-      const html = $(data);
-      trips = html.find('.trip_detail').get().filter(e => $(e).find('img').length);
+      axios.get('https://www.thrillophilia.com/places-to-visit-in-kathmandu')
+      .then(function(resp){
+        var html = $(resp.data);
+        trips = html.find('.trip_detail').get().filter(e => $(e).find('img').length);
 
-      $(trips).find('.count').remove();
-      index = Math.floor(Math.random()*30);
+        $(trips).find('.count').remove();
+        index = Math.floor(Math.random()*30);
 
-      trips.splice(trips.length - 5, 5);
+        trips.splice(trips.length - 5, 5);
 
-      $.each(trips, function(i, trip){
-        $(trip).find('img').each((_, img) => {
-          img.src = img.dataset.normal;
+        $.each(trips, function(i, trip){
+          $(trip).find('img').each((_, img) => {
+            img.src = img.dataset.normal;
+          });
         });
+
+        generateItem(trips);
       });
-
-      generateItem(trips);
-
     } catch (error) {
       console.warn(error);
     }
